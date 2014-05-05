4a6044692cd21ce60cceb137500b748c366a7cb7
Ubuntu 12.04 <neel@ubuntu.(none)>
2014-05-05 11:13:53 -0500

    Removing ability to get to sign_up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Imua::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
