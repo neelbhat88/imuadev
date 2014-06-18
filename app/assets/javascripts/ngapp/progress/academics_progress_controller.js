240a3574be973345895fa87b0b4ffe4a2c4a37e7
Neel Bhat <neelbhat88@gmail.com>
2014-06-18 13:57:57 -0500

    Added the part of the view if a user has classes entered. Using temporary static data for now. Next will come the ability to add classes. #12

--- app/assets/javascripts/ngapp/progress/academics_progress_controller.js
+++ app/assets/javascripts/ngapp/progress/academics_progress_controller.js
@@ -1,4 +1,4 @@
 angular.module('myApp')
 .controller('AcademicsProgressController', ['$scope', function($scope){
-  $scope.user_classes = [];
+  $scope.user_classes = [{name: "Math", grade: "A+"}, {name: "History", grade: "B"}];
 }]);
