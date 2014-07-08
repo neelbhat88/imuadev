407633e6da5c44f9f48342fa43dde8fdea5a518d
ttapers <pat@hokuscholars.org>
2014-07-08 12:19:07 -0500

    Temp fix to verification authenticity stuff
    Allows us to use postman without copying in a token header

--- app/controllers/api/v1/roadmap_controller.rb
+++ app/controllers/api/v1/roadmap_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RoadmapController < ApplicationController
 
   before_filter :authenticate_user!
-
+  skip_before_filter :verify_authenticity_token
   respond_to :json
 
   # POST /roadmap
