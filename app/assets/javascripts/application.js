f412faef9474842a8317265d690aabeac56b4dce
Neel Bhat <neelbhat88@gmail.com>
2014-09-05 15:02:07 -0500

    Adding Angulartics to get some Google Analytics data
    First pass at this to make sure it works, which it does!
    
    Now to remove the JS files I manually downloaded and use bower
    
    #333

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,9 @@
 //= require ladda
 //= require d3
 //= require underscore
+//= require angulartics
+//= require angulartics-ga
+//= require angulartics-debug
 //= require jquery
 //= require jquery_ujs
 //= require jquery-placeholder
