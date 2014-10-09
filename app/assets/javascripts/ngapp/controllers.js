5714d7f5500e2dbe4e9e2c8bd76294709858ba17
Connorsan <jacob.connors3@gmail.com>
2014-10-08 19:05:51 -0500

    Initial commit for navbar.  There are changes in the sessions_controller.rb that I'm unsure of, but put it in there just in case for future use.

--- app/assets/javascripts/ngapp/controllers.js
+++ app/assets/javascripts/ngapp/controllers.js
@@ -3,7 +3,8 @@ angular.module('myApp.controllers', [])
 .controller('HeaderController', ['$scope', 'SessionService',
   function($scope, SessionService) {
     SessionService.getCurrentUser().then(function(user){
-      $scope.user = user;
+      $scope.current_user = user;
+      console.log($scope.current_user)
     });
   }
 ]);
