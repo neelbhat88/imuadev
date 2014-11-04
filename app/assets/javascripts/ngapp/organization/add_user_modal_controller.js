78909bac096541ba3af3bb9a4c78ded15a8ce623
Connorsan <jacob.connors3@gmail.com>
2014-11-04 17:34:21 -0600

    Updated validation style and fixed duplicate email when adding new user fixes #604

--- app/assets/javascripts/ngapp/organization/add_user_modal_controller.js
+++ app/assets/javascripts/ngapp/organization/add_user_modal_controller.js
@@ -1,26 +1,24 @@
 angular.module('myApp')
 .controller('AddUserModalController', ['$scope', '$modalInstance', 'organization', 'new_user', 'UsersService', 'LoadingService',
   function($scope, $modalInstance, organization, new_user, UsersService, LoadingService) {
-    $scope.errors = [];
+    $scope.formErrors = [ '**Please fix the errors above**' ];
     $scope.user = new_user;
+    $scope.class_of = [
+      {name: '-- Graduating Class --', value: null},
+      {name: 'Class of 2014', value: 2014},
+      {name: 'Class of 2015', value: 2015},
+      {name: 'Class of 2016', value: 2016},
+      {name: 'Class of 2017', value: 2017},
+      {name: 'Class of 2018', value: 2018},
+      {name: 'Class of 2019', value: 2019},
+      {name: 'Class of 2020', value: 2020}
+    ]
 
     $scope.add = function($event)
     {
-      $scope.errors = [];
+      $scope.formErrors = [];
 
-      if ($scope.user.email == "")
-        $scope.errors.push("You must provide an email.");
-
-      if ($scope.user.first_name == "")
-        $scope.errors.push("You must provide a first name.");
-
-      if ($scope.user.last_name == "")
-        $scope.errors.push("You must provide a last name.");
-
-      if ($scope.user.is_student && $scope.user.class_of == "0")
-        $scope.errors.push("You must set a graduating class.");
-
-      if ($scope.errors.length == 0)
+      if ($scope.formErrors.length == 0)
       {
         LoadingService.buttonStart($event.currentTarget);
         UsersService.addUser($scope.user)
@@ -28,7 +26,10 @@ angular.module('myApp')
             $modalInstance.close(data.user);
           })
           .error(function(data) {
-            $scope.errors = [data.info]
+            $scope.formErrors.push(data.info);
+            $scope.newUserForm.$invalid = true;
+            $scope.newUserForm.$submitted = true;
+
           })
           .finally(function(){
             LoadingService.buttonStop();
