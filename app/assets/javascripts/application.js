44dbe1301c0b4e645ef47b3ae8b55edc0677868b
Connorsan <jacob.connors3@gmail.com>
2015-02-07 14:10:43 -0600

    Initial WYSIWYG commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,7 @@
 //= require rangy-core
 //= require rangy-selectionsaverestore
 //= require textangular
+//= require textangularsetup
 //= require textangular-sanitize
 //= require spin
 //= require ladda
