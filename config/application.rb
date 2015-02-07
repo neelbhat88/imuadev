6b3f1ca273c0827f28d09b8a1261725fc9497717
Connorsan <jacob.connors3@gmail.com>
2015-02-07 14:28:47 -0600

    Testing shoving glyphicons into asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Imua
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Adding fonts to asset pipeline
+    config.assets.paths << Rails.root.join("vendor","assets", "fonts")
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/app/services)
