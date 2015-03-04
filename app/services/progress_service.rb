35736c0ef400fc52f4a9841e42707310c9347b65
phogan <pat.hogan@konekt.io>
2015-03-03 18:43:25 -0600

    Get rid of old milestone recalculation routine
    #739

--- app/services/progress_service.rb
+++ app/services/progress_service.rb
@@ -70,7 +70,7 @@ class ProgressService
     conditions = Marshal.load(Marshal.dump(params))
 
     if params[:recalculate]
-      get_recalculated_milestones(params[:user_id], params[:time_unit_id], params[:module])
+      recalculate_milestones(conditions)
     end
 
     userQ = Querier.factory(User).select([:role, :time_unit_id, :avatar], [:organization_id]).where(conditions.slice(:user_id))
@@ -129,94 +129,23 @@ class ProgressService
     return ReturnObject.new(:ok, "Progress for organization_id: #{params[:organization_id]}.", view)
   end
 
-  def get_recalculated_milestones(user_id, time_unit_id, module_title)
-    org_milestones = []
-    user_milestones = []
-
-    user = UserRepository.new.get_user(user_id)
-    organization_id = user.organization_id
-
-    # Get org Milestones based on the given parameters
-    conditions = []
-    arguments = {}
-
-    conditions << 'organization_id = :organization_id'
-    arguments[:organization_id] = organization_id
-
-    unless time_unit_id.nil?
-      conditions << 'time_unit_id = :time_unit_id'
-      arguments[:time_unit_id] = time_unit_id
-    end
-
-    unless module_title.nil?
-      conditions << 'module = :module'
-      arguments[:module] = module_title
-    end
-
-    all_conditions = conditions.join(' AND ')
-    org_milestones = Milestone.find(:all, :conditions => [all_conditions, arguments])
-
-    # Get UserMilestones based on the given parameters
-    conditions = []
-    arguments = {}
-
-    conditions << 'user_id = :user_id'
-    arguments[:user_id] = user_id
-
-    unless time_unit_id.nil?
-      conditions << 'time_unit_id = :time_unit_id'
-      arguments[:time_unit_id] = time_unit_id
-    end
-
-    unless module_title.nil?
-      conditions << 'module = :module'
-      arguments[:module] = module_title
-    end
-
-    all_conditions = conditions.join(' AND ')
-    user_milestones = UserMilestone.find(:all, :conditions => [all_conditions, arguments])
-
-    # Get all Milestones for given category and time_unit
-    # Loop through and call has_earned? method
-    # Add to UserMilestone if earned, Delete if not earned and user had previously earned the milestone
-
-    # ToDo: We can probably filter out all the Yes/No milestones here and skip calling has_earned? on all Yes/No milestones
-    # to save a DB call. Yes/No milestone are simple and we know they earned or lost based on checking/unchecking a checkbox so don't need to check again
-    # here when saving other Academics data
-    # For now this is fine, but if DB starts taking a hit we can save some calls here
-    milestones = MilestoneFactory.get_milestone_objects(org_milestones)
-
-    milestones.each do | m |
-      # TODO - has_earned shouldn't depend on time_unit_id
-      earned = m.has_earned?(user, time_unit_id)
-      user_has_milestone = user_milestones.select{|um| um.milestone_id === m.id}.length > 0
-
-      Rails.logger.debug "*****Milestone id: #{m.id}, value: #{m.value} earned? #{earned}"
-      if earned and !user_has_milestone
-        MilestoneService.new(@current_user).add_user_milestone(user_id, time_unit_id, m.id)
-        Rails.logger.debug "*****Milestone added to UserMilestone table"
-      elsif !earned and user_has_milestone
-        MilestoneService.new(@current_user).delete_user_milestone(user_id, time_unit_id, m.id)
-        Rails.logger.debug "*****Milestone deleted from UserMilestone table"
-      end
-    end
-
-    return ReturnObject.new(:ok, "Recalculated milestones for user_id: #{user_id}, time_unit_id: #{time_unit_id}, module_title:#{module_title}.", milestones)
-  end
-
   def recalculate_milestones(params)
     # We use the Querier here because it's easier to deal with when filtering by
     # multiple optional attributes. We take the ids from the Querier results and
-    # perform another set of queries for the ActiveRecord objects, so that they're
-    # more easily plugged into the existing MilestoneFactory, etc. routines.
-    
+    # perform another set of queries for the ActiveRecord objects, so that they
+    # can be plugged into existing MilestoneFactory routines.
+
     conditions = Marshal.load(Marshal.dump(params))
 
     milestoneQ = Querier.factory(Milestone).select([],[:id, :time_unit_id]).where(conditions)
-    userMilestoneQ = Querier.factory(UserMilestone).select([],[:id]).where(conditions.slice(:milestone_id))
+
+    conditions[:milestone_id] = milestoneQ.pluck(:id)
+    userMilestoneQ = Querier.factory(UserMilestone).select([],[:id]).where(conditions)
 
     conditions[:time_unit_id] = milestoneQ.pluck(:time_unit_id)
-    users = User.where(conditions)
+    userQ = Querier.factory(User).select([],[:id]).where(conditions)
+
+    users = User.where(id: userQ.pluck(:id))
     user_milestones = UserMilestone.where(id: userMilestoneQ.pluck(:id))
 
     db_milestones = Milestone.where(id: milestoneQ.pluck(:id))
