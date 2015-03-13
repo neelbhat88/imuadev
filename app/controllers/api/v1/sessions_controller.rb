aa2d0e40fbffa0f0ccaed0dbcda76840f1b98b37
Neel Bhat <neelbhat88@gmail.com>
2015-03-13 16:16:12 -0500

    Skipping XSRF check in Sessions controller also
    #789

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SessionsController < Devise::SessionsController
 	respond_to :json
-
+	skip_before_filter :verify_authenticity_token
+	
 	# POST /user/sign_in
 	# This is called when we log the user in with their
 	# email and password AS WELL AS by the angular-devise
@@ -13,26 +14,12 @@ class Api::V1::SessionsController < Devise::SessionsController
 		email = params[:user][:email] || request.headers["X-API-EMAIL"].presence
 		password = params[:user][:password]
 
-		# TODO: Try and tell warden to not set session cookie
-		# self.resource = warden.authenticate!(auth_options)
-		# sign_in(resource_name, resource)
-		# yield resource if block_given?
-
-		Rails.logger.debug("****** Email #{email}")
-		Rails.logger.debug("****** Token from request #{user_auth_token}")
-		Rails.logger.debug("****** Current User is #{current_user.email} : #{current_user.first_name}") if current_user
-
 		result = nil
 		if password.nil?
-			Rails.logger.debug("****** Token Authentication")
 			result = authenticate_with_token({email: email, access_token: user_auth_token})
 		else
-			Rails.logger.debug("****** Credentials Authentication")
 			result = authenticate_with_credentials({email: email, password: password})
 		end
-
-		Rails.logger.debug("********* Current User is #{current_user.email} : #{current_user.first_name}") if current_user
-		Rails.logger.debug("********* After auth user's token is #{current_user.access_token.token_value}") if current_user && current_user.access_token
 
 		if result[:status] == 200
 			render status: 200,
@@ -52,10 +39,7 @@ class Api::V1::SessionsController < Devise::SessionsController
 		user_auth_token = request.headers["X-API-TOKEN"]
 		email = request.headers["X-API-EMAIL"]
 
-		#warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
-
 		user = email && User.find_by_email(email)
-		Rails.logger.debug("********* Destroying user's access token: user - #{user.email}") if user
 
 		if user.nil?
 			render status: 401,
@@ -65,16 +49,10 @@ class Api::V1::SessionsController < Devise::SessionsController
 			return
 		end
 
-		Rails.logger.debug("********* Destroying user's access token - #{user.access_token}")
 		# Destroy user's access token
 		user.access_token.destroy if user.access_token
 
-		Rails.logger.debug("********* Destroyed user's access token - #{user.access_token}")
-		Rails.logger.debug("********* Current User is #{current_user.email} : #{current_user.first_name}") if current_user
-
 		sign_out
-
-		Rails.logger.debug("********* Current User is #{current_user.email} : #{current_user.first_name}") if current_user
 
 		render :status => 200,
 					:json => { :success => true,
@@ -114,16 +92,14 @@ private
 
 		user = User.find_by_email(email)
 
-		Rails.logger.debug("******** #{email}")
-		Rails.logger.debug("******** #{password}")
 		if user.nil? || !user.valid_password?(password)
 			return {status: 401, error: "Invalid email or password"}
 		end
 
 		sign_in(user, store: false)
-		Rails.logger.debug("******** AFter devise sign_in")
+
 		access_token = AccessTokenCreator.new.generate_token(user)
-		Rails.logger.debug("******** New access token #{access_token}")
+
 		Background.process do
 	    Keen.publish("user_engagement", {
 	                                      :user => current_user,
@@ -150,7 +126,7 @@ private
     end
 
 		sign_in(user, store: false)
-		Rails.logger.debug("********* Current User is #{current_user.email}")
+
 		#TODO: Look into why creating a new one every time we hit this doesn't work
 		#access_token = AccessTokenCreator.new.generate_token(user)
 
