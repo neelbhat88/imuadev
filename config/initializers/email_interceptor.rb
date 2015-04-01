e80b6181f50ca724882547e55348815de881092a
Neel Bhat <neelbhat88@gmail.com>
2015-04-01 15:54:20 -0500

    Updating how Oink logs to heroku

--- config/initializers/email_interceptor.rb
+++ config/initializers/email_interceptor.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
 ActionMailer::Base.register_interceptor(ProductionMailInterceptor) if Rails.env.production?
 
-Rails.application.middleware.use( Oink::Middleware, :logger => Rails.logger )
+Rails.application.middleware.use( Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT) )
