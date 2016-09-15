43fc3a7f20c076cdbd0c5d5b8678628418f4ff75
Neel Bhat <neelbhat88@gmail.com>
2016-09-15 11:16:39 -0400

    Concatenating JS and CSS to make development faster using Docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,11 +51,10 @@ Imua::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Do not compress assets
   config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.compile = true
+  config.assets.digest = false
+  config.assets.debug = false
 
   # Tell Unicorn to log just like WEBbrick and Thin web servers do
   config.logger = Logger.new(STDOUT)
